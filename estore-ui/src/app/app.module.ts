76a5b16e5fa2ec3e31ac61bf22769ced39d4c37a
jcampbell11245 <jc7305@rit.edu>
2022-02-25 15:56:41 -0500

    Added search bar component and edited HTML/CSS
    HTML and CSS contains centered search bar and submit button that redirects to search url.

--- estore-ui/src/app/app.module.ts
+++ estore-ui/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomepageComponent } from './homepage/homepage.component';
 import { StoreComponent } from './store/store.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    StoreComponent
+    StoreComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
