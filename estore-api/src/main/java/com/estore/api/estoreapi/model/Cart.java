ea03b51ba4172b0c62795beabd2d5429dab2c8fa
kara_kolodinsky <ktk4111@rit.edu>
2022-03-08 15:35:37 -0500

    cart initialized

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Cart.java
@@ -0,0 +1,9 @@
+package com.estore.api.estoreapi.model;
+import com.estore.api.estoreapi.*;
+import java.utils.List;
+
+public class Cart {
+    public Cart(Item[] items) {
+        List<Items>  items = new ArrayList<Item>();
+    }
+}
