ae789f7417d88764b0b60332a54877bb8eb3304c
Alex Hetrick <aph1777@rit.edu>
2022-03-11 18:04:38 -0500

    Created a REST API for user login information

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/UserController.java
@@ -0,0 +1,197 @@
+package com.estore.api.estoreapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.estore.api.estoreapi.persistence.UserDAO;
+import com.estore.api.estoreapi.model.User;
+
+/**
+ * Handles the REST API requests for the user resource.
+ * 
+ * {@literal @}RestController spring annotation identifies this class as a rest api
+ * method handler to the spring framework.
+ * 
+ * @author Alex Hetrick
+ */
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    private static final Logger LOG = Logger.getLogger(UserController.class.getName());
+    private UserDAO userDAO;
+
+    /**
+     * Creates a rest api controller to reponds to requests.
+     * 
+     * @param UserDAO the {@link UserDAO user data access object} to perform CRUD operations
+     */
+    public UserController(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    /**
+     * Responds to the GET request for a {@linkplain User user} for the given id.
+     * 
+     * @param id The id used to locate the {@link User user}
+     * 
+     * @return ResponseEntity with {@link User user} object and HTTP status of OK if found
+     * ResponseEntity with HTTP status of NOT_FOUND if not found
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable int id) {
+        LOG.info("GET /users/" + id);
+        try {
+            User user = userDAO.getUser(id);
+            if (user != null)
+                return new ResponseEntity<User>(user, HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Responds to the GET request for all {@linkplain User users}
+     * 
+     * @return ResponseEntity with array of {@link User user} objects (may be empty) and
+     * HTTP status of OK<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("")
+    public ResponseEntity<User[]> getUsers() {
+        LOG.info("GET /users");
+        try{
+            User[] users = userDAO.getUsers();
+            if (users != null)
+                return new ResponseEntity<User[]>(users, HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * responds to the GET request for all {@linkplain User users} whose name contains
+     * the text in name.
+     * 
+     * @param name the name parameter which contains the text used to find the {@link User users}
+     * 
+     * @return ResponseEntity with array of {@link User user} objects (may be empty) and
+     * HTTP status of OK
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * 
+     */
+    @GetMapping("/")
+    public ResponseEntity<User> searchUsers(@RequestParam String username) {
+        LOG.info("GET /users/?name= " + username);
+        try {
+            User user = userDAO.findUser(username);
+            if (user != null)
+                return new ResponseEntity<User>(user, HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Creates a {@linkplain User user} with the provided user object.
+     * 
+     * @param user - The {@link User user} to create
+     * 
+     * @return ResponseEntity with created {@link User user} object and HTTP status of CREATED<br>
+     * ResponseEntity with HTTP status of CONFLICT if {@link User user} object already exists<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PostMapping("")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        LOG.info("POST /users/ " + user);
+        try {
+            User created_user = userDAO.createUser(user);
+            if (created_user != null){
+                return new ResponseEntity<User>(created_user,HttpStatus.OK);
+            }
+            else {
+                return new ResponseEntity<User>(HttpStatus.CONFLICT);
+            }
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Updates the {@linkplain User user} with the provided {@linkplain User user} object, if it exists.
+     * 
+     * @param user the {@link User user} to update
+     * 
+     * @return ResponseEntity with updated {@link User user} object and HTTP status of OK if updated
+     * ResponseEntity with HTTP status of NOT_FOUND if not found
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PutMapping("")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        LOG.info("PUT /users " + user);
+        try {
+            User updatedUser = userDAO.updateUser(user);
+            if (updatedUser != null)
+                return new ResponseEntity<User>(updatedUser, HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Deletes a {@linkplain User user} with the given id
+     * 
+     * @param id The id of the {@link User user} to deleted
+     * 
+     * @return ResponseEntity HTTP status of OK if deleted<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable int id) {
+        LOG.info("DELETE /users/" + id);
+        try {
+            if(userDAO.deleteUser(id)) {
+                return new ResponseEntity<User>(HttpStatus.OK);
+            }
+            else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/UserFileDAO.java
@@ -0,0 +1,226 @@
+package com.estore.api.estoreapi.persistence;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.estore.api.estoreapi.model.User;
+
+/**
+ * Implements the functionality for JSON file-based peristance for Users
+ * 
+ * {@literal @}Component Spring annotation instantiates a single instance of this
+ * class and injects the instance into other classes as needed
+ * 
+ * @author Alex Hetrick
+ */
+@Component
+public class UserFileDAO implements UserDAO {
+    Map<Integer, User> users;           // Provides a local cache of the item objects
+    private ObjectMapper objectMapper;  // Provides conversion between User
+                                        // objects and JSON text format written
+                                        // to the file
+    private static int nextId;          // The next Id to assign to a new User
+    private String filename;            // Filename to read from and write to
+
+    /**
+     * Creates an User File Data Access Object
+     * 
+     * @param filename      Filename to read from and write to
+     * @param objectMapper  Provides JSON Object to/from Java Object serialization and deserialization
+     * 
+     * @throws IOException when file cannot be accessed or read from
+     */
+    public UserFileDAO(@Value("${users.file}") String filename, ObjectMapper objectMapper) throws IOException {
+        this.filename = filename;
+        this.objectMapper = objectMapper;
+        load();  // load the Users from the file
+    }
+
+    /**
+     * Generates the next id for a new {@linkplain User user}
+     * 
+     * @return The next id
+     */
+    private synchronized static int nextId() {
+        int id = nextId;
+        ++nextId;
+        return id;
+    }
+
+    /**
+     * Generates an array of {@linkplain User users} from the tree map
+     * 
+     * @return  The array of {@link User users}, may be empty
+     */
+    private User[] getUsersArray() {
+        ArrayList<User> userArrayList = new ArrayList<>();
+
+        for (User user : users.values()) {
+            userArrayList.add(user);
+        }
+
+        User[] userArray = new User[userArrayList.size()];
+        userArrayList.toArray(userArray);
+        return userArray;
+    }
+
+    /**
+     * Saves the {@linkplain User users} from the map into the file as an array of JSON objects.
+     * 
+     * @return true if the {@link User users} were written successfully
+     * 
+     * @throws IOException when file cannot be accessed or written to
+     */
+    private boolean save() throws IOException {
+        User[] userArray = getUsersArray();
+
+        // Serializes the Java Objects to JSON objects into the file
+        // writeValue will thrown an IOException if there is an issue
+        // with the file or reading from the file
+        objectMapper.writeValue(new File(filename),userArray);
+        return true;
+    }
+
+    /**
+     * Loads {@linkplain User user} from the JSON file into the map;
+     * also sets next id to one more than the greatest id found in the file.
+     * 
+     * @return true if the file was read successfully
+     * 
+     * @throws IOException when file cannot be accessed or read from
+     */
+    private boolean load() throws IOException {
+        users = new TreeMap<>();
+        nextId = 0;
+
+        // deserializes the JSON objects from the file into an array of Users;
+        // readValue will throw an IOException if there's an issue with the file
+        // or reading from the file.
+        User[] userArray = objectMapper.readValue(new File(filename), User[].class);
+
+        // add each User to the tree map and keep track of the greatest id
+        for (User user : userArray) {
+            users.put(user.getId(), user);
+            if (user.getId() > nextId)
+                nextId = user.getId();
+        }
+        // make the next id one greater than the maximum from the file
+        ++nextId;
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public User[] getUsers() throws IOException {
+        synchronized(users) {
+            return getUsersArray();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public User getUser(int id) throws IOException {
+        synchronized(users) {
+            if (users.containsKey(id))
+                return users.get(id);
+            else
+                return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public User findUser(String username) throws IOException {
+        synchronized(users) {
+            User[] userArray = getUsersArray();
+            for (User user : userArray)
+            {
+                if (user.getUsername().equals(username)) {
+                    return user;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public User createUser(User user) throws IOException {
+        synchronized(users) {
+            // We create a new user object because the id field is immutable
+            // and we need to assign the next unique id
+            if (usernameExists(user.getUsername()) == false){
+                User newUser = new User(nextId(), user.getUsername(), user.getPassword());
+                users.put(newUser.getId(), newUser);
+                save(); // may throw an IOException
+                return newUser;
+            }
+            else {
+                return null;
+            }
+            
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public User updateUser(User user) throws IOException {
+        synchronized(users) {
+            if (users.containsKey(user.getId()) == false)
+                return null;  // item does not exist
+
+                users.put(user.getId(), user);
+            save(); // may throw an IOException
+            return user;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean deleteUser(int id) throws IOException {
+        synchronized(users) {
+            if (users.containsKey(id)) {
+                users.remove(id);
+                return save();
+            }
+            else
+                return false;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean usernameExists(String username) throws IOException {
+        synchronized(users) {
+            for (User stock_item: users.values()){
+                if (stock_item.getUsername().equals(username)){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    
+}
