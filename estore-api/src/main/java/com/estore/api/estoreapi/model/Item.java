04ec6d49afff15f460f73c2ee34176dade63e872
kara_kolodinsky <ktk4111@rit.edu>
2022-03-11 20:37:16 -0500

    added price to string

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Item.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Item.java
@@ -13,7 +13,7 @@ public class Item {
     private static final Logger LOG = Logger.getLogger(Item.class.getName());
 
     // Package private for tests
-    static final String STRING_FORMAT = "Item [id=%d, name=%s, stock=%s]";
+    static final String STRING_FORMAT = "Item [id=%d, name=%s, stock=%s, price=%s]";
 
     @JsonProperty("id") private int id;
     @JsonProperty("name") private String name;
