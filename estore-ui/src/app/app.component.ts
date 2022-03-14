657a24f866a7178797521efe456a5a43ce9a4a40
Alex Hetrick <aph1777@rit.edu>
2022-03-14 00:55:36 -0400

    Added avatar dropdown menu functionality

--- estore-ui/src/app/app.component.ts
+++ estore-ui/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 import { AuthService } from './auth.service';
 
@@ -9,13 +15,38 @@ import { AuthService } from './auth.service';
 })
 export class AppComponent {
   title = 'Artist E-Store';
-  isLoggedIn: boolean = false;
 
-  constructor(private authService: AuthService) {
+  isLoggedIn: boolean = false;
+  accountDropdownToggle: boolean = false;
+
+  @ViewChild('dropdown') dropdown?: ElementRef;
+  @ViewChild('avatar') avatar?: ElementRef;
+
+  constructor(private authService: AuthService, private renderer: Renderer2) {
     this.authService.getAuthStateObs().subscribe((x) => (this.isLoggedIn = x));
+
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (
+        this.avatar != undefined &&
+        this.dropdown != undefined &&
+        e.target !== this.avatar.nativeElement &&
+        e.target !== this.dropdown.nativeElement
+      ) {
+        this.setAccountDropdown(false);
+      }
+    });
+  }
+
+  toggleAccountDropdown(): void {
+    this.accountDropdownToggle = !this.accountDropdownToggle;
+  }
+
+  setAccountDropdown(val: boolean): void {
+    this.accountDropdownToggle = val;
   }
 
   logout(): void {
     this.authService.updateLoginState(undefined);
+    this.setAccountDropdown(false);
   }
 }
