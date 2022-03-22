8b22db83dfaecf1625f1fb61e72dcddb6cbf93e3
Daniel Kench <djk5788@rit.edu>
2022-03-21 20:41:44 -0400

    Created basic layout of checkout page

--- estore-ui/src/app/checkout/checkout.component.ts
+++ estore-ui/src/app/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from '../cartitem';
+import { CartItemService } from '../cartitem.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  cart: CartItem[] = [];
 
-  constructor() { }
+  constructor(private CartitemService: CartItemService) { }
 
   ngOnInit(): void {
+    this.getItems()
+  }
+
+  getItems(): void {
+    this.CartitemService.getCart()
+      .subscribe(cart => this.cart = cart);
   }
 
 }
