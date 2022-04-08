0986c40d2ddd42c79f6a7520da12561b9d661489
kara_kolodinsky <ktk4111@rit.edu>
2022-04-08 18:46:09 -0400

    Update store.component.ts

--- estore-ui/src/app/store/store.component.ts
+++ estore-ui/src/app/store/store.component.ts
@@ -28,11 +28,10 @@ export class StoreComponent implements OnInit {
   }
 
   add(item: Item): void {
-    if (item.stock> 0){
+    // chek if item.stock> cart.item.quantity ???? cant figure out how to do taht
       this.cartService
       .addCartItem(this.authService.getCurrentUser().username, item)
       .subscribe();
-    }
   }
 
   adminLoggedIn(): boolean {
