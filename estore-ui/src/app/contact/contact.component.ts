6be7b81ac515cae01fa5887e3e563cc841274df2
Alex Hetrick <aph1777@rit.edu>
2022-04-20 22:22:01 -0400

    Fixed issues brought up by SonarScanner

--- estore-ui/src/app/contact/contact.component.ts
+++ estore-ui/src/app/contact/contact.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    // constructor
   }
 
+  ngOnInit(): void {
+    // on init
+  }
 }
--- estore-ui/src/app/homepage/homepage.component.ts
+++ estore-ui/src/app/homepage/homepage.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    // constructor
   }
 
+  ngOnInit(): void {
+    // on init
+  }
 }
